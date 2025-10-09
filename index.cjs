4d3cae2c6e914f8e8f298496c04c062e70dab572
mydkong <contact.donkeykong@protonmail.com>
2025-10-09 21:47:40 +0200

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const token=process.env.TOKEN
-const { Client, GatewayIntentBits, SlashCommandBuilder, REST, Routes, EmbedBuilder, ChannelType, PermissionFlagsBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, ActivityType, ModalBuilder, TextInputBuilder, TextInputStyle, StringSelectMenuBuilder, MessageFlags, InteractionType, TextDisplayBuilder, ContainerBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, SlashCommandBuilder, REST, Routes, EmbedBuilder, ChannelType, PermissionFlagsBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, ActivityType, ModalBuilder, TextInputBuilder, TextInputStyle, StringSelectMenuBuilder, MessageFlags, InteractionType, TextDisplayBuilder, ContainerBuilder, Events } = require('discord.js');
 const fs = require('fs');
 const marked=require('marked');
 const client=new Client({
@@ -32,7 +32,7 @@ var queue=JSON.parse(dataQueue)
 
 const commands=[
   new SlashCommandBuilder()
-    .setName('show-embed')
+    .setName('tickets_show-container')
     .setDescription('Affiche l\'embed dans le salon où la commande est exécutée')
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator), // only admins can see that
 ].map(command => command.toJSON());
@@ -51,5 +51,25 @@ const rest = new REST({ version: '10' }).setToken(token);
 		console.error(error);
 	}
 })('1425714937547984947'); // auto-executed function to put the following commands
+
+client.on(Events.ClientReady, ()=>{
+	console.log('The bot is connected!')
+})
+
+client.on(Events.InteractionCreate, async interaction=>{
+	if(!interaction.isChatInputCommand())return;
+	if(ineraction.commandName==='tickets_show-container'){
+		await interaction.deferReply({
+			ephemeral: true
+		})
+		const text=new TextDisplayBuilder().setContent("### Want to contact the staff? You're in the right place!\nYou've **four** available ticket types.\nChoose one to create a ticket <:pepewow:1400572079585362054>!")
+		const container=new ContainerBuilder()
+			.addTextDisplayComponents(text);
+		await interaction.editReply({
+			flags: MessageFlags.IsComponentsV2,
+			components: [container]
+		})
+	}
+})
 
 client.login(token)
