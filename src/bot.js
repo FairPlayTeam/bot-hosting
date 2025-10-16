cce2e3e1947bb4cc096c6ee1faab36e443b9a80d
mydkong <contact.donkeykong@protonmail.com>
2025-10-16 07:43:37 +0200

    Adding ban command module to bot.js

--- src/bot.js
+++ src/bot.js
@@ -13,6 +13,7 @@ import { LANG } from './constants.js'
 import * as vanish from './commands/vanish.js'
 import * as ticketsShow from './commands/tickets_show-container.js'
 import * as ticketsConfig from './commands/tickets_config.js'
+import * as ban from './commands/ban.js'
 
 import { routeInteraction } from './interactions/router.js'
 import { onMessageCreate } from './events/messageCreate.js'
@@ -37,7 +38,7 @@ const client = new Client({
 
 const store = new JsonStore('data.json')
 
-const commandModules = [vanish, ticketsShow, ticketsConfig]
+const commandModules = [vanish, ticketsShow, ticketsConfig, ban]
 const commands = commandModules.map(c => c.data.toJSON())
 
 const rest = new REST({ version: '10' }).setToken(token)
