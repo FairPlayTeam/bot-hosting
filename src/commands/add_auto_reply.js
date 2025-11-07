bb2d8f59b5ce586ac30965f40adfa0caca1241f7
somebody <132383273+NaFo44@users.noreply.github.com>
2025-11-07 11:55:35 +0100

    Sanitize input in add_auto_reply command
    Refactor auto-reply command to sanitize input and improve code structure.

--- src/commands/add_auto_reply.js
+++ src/commands/add_auto_reply.js
@@ -1,29 +1,44 @@
 import {
 	SlashCommandBuilder,
 	PermissionFlagsBits,
-    MessageFlags,
+	MessageFlags,
 } from 'discord.js'
 import { store } from '../bot.js'
 
+// sanitize
+function sanitizeInput(input) {
+	return input
+		.replace(/@everyone/gi, '[everyone]')
+		.replace(/@here/gi, '[here]')
+		.replace(/<@!?(\d+)>/g, '[user]')
+		.replace(/<@&(\d+)>/g, '[role]')
+		.trim()
+}
+
 export const data = new SlashCommandBuilder()
-  .setName('add_an_auto-reply')
-  .setDescription('Add a reaction that the bot will send when it detects a word')
-  .addStringOption(option => option.setName('word')
-                                 .setDescription('The word to be detected')
-								 .setRequired(true)
-  )
-  .addStringOption(option => option.setName('message')
-                                 .setDescription('The message to send')
-								 .setRequired(true)
-  )
+	.setName('add_an_auto-reply')
+	.setDescription('Add a reaction that the bot will send when it detects a word')
+	.addStringOption(option =>
+		option
+			.setName('word')
+			.setDescription('The word to be detected')
+			.setRequired(true)
+	)
+	.addStringOption(option =>
+		option
+			.setName('message')
+			.setDescription('The message to send')
+			.setRequired(true)
+	)
 
 export const execute = async interaction => {
-    await interaction.deferReply()
-    const word = interaction.options.getString("word")
-    const message = interaction.options.getString('message')
-    store.addAutoReply(interaction.guild.id,word, message )
-    await interaction.editReply({ content : "Successfully added !" })
-    
-    
-    return
+	await interaction.deferReply({ flags: MessageFlags.Ephemeral })
+	const rawWord = interaction.options.getString('word')
+	const rawMessage = interaction.options.getString('message')
+
+	const word = sanitizeInput(rawWord)
+	const message = sanitizeInput(rawMessage)
+
+	store.addAutoReply(interaction.guild.id, word, message)
+	await interaction.editReply({ content: 'Successfully added!' })
 }
